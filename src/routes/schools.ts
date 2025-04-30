521f55cb3f26e4a35e8ad475248597e64964c188
Gaston Chenet <contact@gastonchenet.com>
2025-04-30 02:49:03 +0200

    Added school validation route

--- src/routes/schools.ts
+++ src/routes/schools.ts
@@ -26,7 +26,110 @@ type SchoolData = {
   stripeSubscriptionId: string;
 };
 
+type SiretReponseHeader = {
+  statut: number;
+  message: string;
+};
+
+type SiretResponseEtablissementUniteLegale = {
+  etatAdministratifUniteLegale: string;
+  statutDiffusionUniteLegale: string;
+  categorieJuridiqueUniteLegale: string;
+  denominationUniteLegale: string;
+  sigleUniteLegale: string | null;
+  denominationUsuelle1UniteLegale: string | null;
+  denominationUsuelle2UniteLegale: string | null;
+  denominationUsuelle3UniteLegale: string | null;
+  sexeUniteLegale: string | null;
+  nomUniteLegale: string | null;
+  nomUsageUniteLegale: string | null;
+  prenom1UniteLegale: string | null;
+  prenom2UniteLegale: string | null;
+  prenom3UniteLegale: string | null;
+  prenom4UniteLegale: string | null;
+  prenomUsuelUniteLegale: string | null;
+  activitePrincipaleUniteLegale: string;
+  nomenclatureActivitePrincipaleUniteLegale: string;
+  identifiantAssociationUniteLegale: string | null;
+  economieSocialeSolidaireUniteLegale: string | null;
+  societeMissionUniteLegale: string | null;
+  caractereEmployeurUniteLegale: string | null;
+  trancheEffectifsUniteLegale: string | null;
+  anneeEffectifsUniteLegale: string | null;
+  nicSiegeUniteLegale: string | null;
+  dateDernierTraitementUniteLegale: string;
+  categorieEntreprise: string | null;
+  anneeCategorieEntreprise: string | null;
+};
+
+type SiretResponseAdresseEtablissement = {
+  complementAdresseEtablissement: string | null;
+  numeroVoieEtablissement: string | null;
+  indiceRepetitionEtablissement: string | null;
+  dernierNumeroVoieEtablissement: string | null;
+  indiceRepetitionDernierNumeroVoieEtablissement: string | null;
+  typeVoieEtablissement: string | null;
+  libelleVoieEtablissement: string | null;
+  codePostalEtablissement: string | null;
+  libelleCommuneEtablissement: string | null;
+  libelleCommuneEtrangerEtablissement: string | null;
+  distributionSpecialeEtablissement: string | null;
+  codeCommuneEtablissement: string | null;
+  codeCedexEtablissement: string | null;
+  libelleCedexEtablissement: string | null;
+  codePaysEtrangerEtablissement: string | null;
+  libellePaysEtrangerEtablissement: string | null;
+  identifiantAdresseEtablissement: string | null;
+  coordonneeLambertAbscisseEtablissement: string | null;
+  coordonneeLambertOrdonneeEtablissement: string | null;
+};
+
+type SiretResponsePeriodeEtablissement = {
+  dateFin: null;
+  dateDebut: string;
+  etatAdministratifEtablissement: string;
+  changementEtatAdministratifEtablissement: boolean;
+  enseigne1Etablissement: string | null;
+  enseigne2Etablissement: string | null;
+  enseigne3Etablissement: string | null;
+  changementEnseigneEtablissement: boolean;
+  denominationUsuelleEtablissement: string | null;
+  changementDenominationUsuelleEtablissement: boolean;
+  activitePrincipaleEtablissement: string;
+  nomenclatureActivitePrincipaleEtablissement: string;
+  changementActivitePrincipaleEtablissement: boolean;
+  caractereEmployeurEtablissement: string;
+  changementCaractereEmployeurEtablissement: boolean;
+};
+
+type SiretResponseEtablissement = {
+  siren: string;
+  nic: string;
+  siret: string;
+  statutDiffusionEtablissement: string;
+  dateCreationEtablissement: string;
+  trancheEffectifsEtablissement: string | null;
+  anneeEffectifsEtablissement: string | null;
+  etablissementSiege: boolean;
+  nombrePeriodesEtablissement: number;
+  uniteLegale: SiretResponseEtablissementUniteLegale;
+  adresseEtablissement: SiretResponseAdresseEtablissement;
+  adresse2Etablissement: SiretResponseAdresseEtablissement | null;
+  periodesEtablissement: SiretResponsePeriodeEtablissement[];
+};
+
+type SiretResponse = {
+  header: SiretReponseHeader;
+  etablissement: SiretResponseEtablissement;
+};
+
+const TOLERATED_ACTIVITY = {
+  "85.52Z": "Enseignement culturel",
+  "90.01Z": "Arts du spectacle vivant",
+};
+
 const schoolsMap = new Map<string, SchoolData>();
+const siretResponseCache = new Map<string, Response>();
 
 router.post(
   "/",
@@ -137,6 +240,63 @@ router.post(
     }
   }
 );
+
+router.get("/validate/siret/:siret", async (req, res) => {
+  if (!req.params.siret) {
+    res.status(400).json({ error: "SIRET number is required" });
+    return;
+  }
+
+  let response = siretResponseCache.get(req.params.siret);
+
+  if (!response) {
+    response = await fetch(
+      `https://api.insee.fr/api-sirene/3.11/siret/${req.params.siret}`,
+      {
+        method: "GET",
+        headers: {
+          "X-INSEE-Api-Key-Integration": process.env.INSEE_SIREN_API_KEY!,
+          Accept: "application/json",
+        },
+      }
+    );
+  }
+
+  if (!response.ok) {
+    res.status(400).json({ error: "Invalid SIRET number", code: 601 });
+    return;
+  }
+
+  const data: SiretResponse = await response.json();
+
+  if (
+    !(
+      data.etablissement.uniteLegale.activitePrincipaleUniteLegale in
+      TOLERATED_ACTIVITY
+    )
+  ) {
+    res.status(400).json({
+      error: "The activity of the establishment is not tolerated",
+      code: 602,
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    name: data.etablissement.uniteLegale.denominationUniteLegale,
+    dateCreation: data.etablissement.dateCreationEtablissement,
+    activityCode: data.etablissement.uniteLegale.activitePrincipaleUniteLegale,
+    activityLabel: (TOLERATED_ACTIVITY as Record<string, string>)[
+      data.etablissement.uniteLegale.activitePrincipaleUniteLegale
+    ],
+    address: {
+      street: data.etablissement.adresseEtablissement.libelleVoieEtablissement,
+      zipCode: data.etablissement.adresseEtablissement.codePostalEtablissement,
+      city: data.etablissement.adresseEtablissement.libelleCommuneEtablissement,
+    },
+  });
+});
 
 router.post(
   "/payment/webhook",
