aabf5b7b089ae704fb29d4a8f85b7d4bf197346c
Thomas <thomas.renault0624@gmail.com>
2025-04-30 15:30:27 +0200

    Add Joi validation schemas for creating and updating teachers; refactor routes to use validation middleware

--- src/utils/joiValidation.ts
+++ src/utils/joiValidation.ts
@@ -5,7 +5,10 @@ export function validateBody(schema: Joi.ObjectSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
     const { error, value } = schema.validate(req.body);
     if (error) {
-      return res.status(400).json({ message: error.details[0].message });
+      res
+        .status(400)
+        .json({ message: "Validation Error : " + error.details[0].message });
+      return;
     }
     req.body = value;
     next();
