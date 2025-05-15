e4c574bffba24924f2da4714560fb6b1c3610c89
Gaston Chenet <contact@gastonchenet.com>
2025-05-15 15:06:38 +0200

    Updated email sent verification

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -186,7 +186,7 @@ router.post(
       expiresIn: "10m",
     });
 
-    await sendEmail(
+    const sent = await sendEmail(
       user.email,
       "Réinitialisation de mot de passe",
       /* HTML */ `
@@ -206,6 +206,11 @@ router.post(
         <p>L'équipe de VLAD</p>
       `
     );
+
+    if (!sent) {
+      res.status(401).json({ message: "Failed to send email" });
+      return;
+    }
 
     res.status(200).json({
       message: "Password reset token generated",
